e4263489970d80e59713f051e689f50b094cf4d2
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-17 19:12:55 +0530

    17-01-p117

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("81.to add two digits to a given positive integer of length two."+"<br>");
+function test81(x){
+    return x%10+Math.floor(x/10);
+}
+document.write("The number :13 , Ans :"+test81(13)+"<br><br>");
